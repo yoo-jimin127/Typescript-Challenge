0217457e28e6d16aa454675c9b2bc534f2702245
yoo-jimin127 <dbwlals9936@naver.com>
2022-10-17 16:35:45 +0900

    add : upload sample js & ts file

---
+++ ts-fundamental-source/why-ts/sample.js
@@ -0,0 +1,14 @@
+// type declaration
+
+// @ts-check
+
+/**
+ * @param {number} a first-parameter
+ * @param {number} b second-parameter
+ */
+
+function sum(a, b) {
+    return a + b;
+}
+
+sum(10, 20);
---
+++ ts-fundamental-source/why-ts/sample.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+let result: number = add(10, 20);
+result.toLocaleString();

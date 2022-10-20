46849d6ad9ed65e1cba8fe36b38811b084b0f317
yoo-jimin127 <dbwlals9936@naver.com>
2022-10-20 14:44:02 +0900

    fix : add type & declare function (except object typing

--- ts-fundamental-source/04.toy-project/04-1.todo/src/index.ts
+++ ts-fundamental-source/04.toy-project/04-1.todo/src/index.ts
@@ -34,17 +34,26 @@ function logFirstTodo(): object {
   return todoItems[0];
 }
 
-function showCompleted() {
+function showCompleted(): object[] {
   return todoItems.filter(item => item.done);
 }
 
-// TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
-  // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+function addTwoTodoItems(): void {
+  const item1 = {
+    id: 4,
+    title: '아이템 4',
+    done: false,
+  }
+  addTodo(item1);
+  addTodo({
+    id: 5,
+    title: '아이템 5',
+    done: false,
+  });
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 

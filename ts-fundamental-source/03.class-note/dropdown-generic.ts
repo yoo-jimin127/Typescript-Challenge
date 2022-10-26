ab0ea6a0221899b5a546074a040777819fdf770f
yoo-jimin127 <dbwlals9936@naver.com>
2022-10-26 11:00:25 +0900

    fix : using union type

--- ts-fundamental-source/03.class-note/dropdown-generic.ts
+++ ts-fundamental-source/03.class-note/dropdown-generic.ts
@@ -1,16 +1,26 @@
-const emails = [
+interface Email {
+    value: string;
+    selected: boolean;
+}
+
+const emails: Email[] = [
     {value: 'naver.com', selected: true},
     {value: 'gmail.com', selected: false},
     {value: 'hanmail.net', selected: false},
 ];
 
-const numberOfProducts = [
+interface ProductNumber {
+    value: number;
+    selected: boolean;
+}
+
+const numberOfProducts: ProductNumber[] = [
     {value: 1, selected: true},
     {value: 2, selected: false},
     {value: 3, selected: false},
 ]
 
-function createDropDownItem(item: {value: string; selected: boolean}) {
+function createDropDownItem(item: {value: string; selected: boolean} | { value: string; selected: boolean}) {
     const option = document.createElement('option');
     option.value = item.value.toString();
     option.innerText = item.value.toString();
@@ -18,7 +28,7 @@ function createDropDownItem(item: {value: string; selected: boolean}) {
     return option;
 }
 
-emails.forEach(function (email) {
+emails.forEach(function (email: {value: string; selected: boolean}) {
     const item = createDropDownItem(email);
     const selectTag = document.querySelector('#email-dropdown');
     selectTag.appendChild(item);

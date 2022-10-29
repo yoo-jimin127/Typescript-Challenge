a58383d4427aac97d59535a18c49af539e54cba0
yoo-jimin127 <dbwlals9936@naver.com>
2022-10-29 20:21:31 +0900

    add : 04-2.address-book source code

--- ts-fundamental-source/04.toy-project/04-2.address-book/src/index.ts
+++ ts-fundamental-source/04.toy-project/04-2.address-book/src/index.ts
@@ -10,6 +10,12 @@ interface Contact {
     phones: PhoneNumberDictionary;
 }
 
+enum PhoneType {
+    Home = 'home',
+    Office = 'office',
+    Studio = 'studio'
+}
+
 // mocking api
 function fetchContacts(): Promise<Contact[]> {
     const contacts: Contact[] = [
@@ -55,43 +61,43 @@ function fetchContacts(): Promise<Contact[]> {
 
 // main
 class AddressBook {
-    // TODO: 아래 변수의 타입을 지정해보세요.
-    contacts = [];
+    contacts: Contact[] = [];
 
     constructor() {
         this.fetchData();
     }
 
-    fetchData() {
+    fetchData(): void {
         fetchContacts().then(response => {
             this.contacts = response;
         });
     }
 
-    /* TODO: 아래 함수들의 파라미터 타입과 반환 타입을 지정해보세요 */
-    findContactByName(name) {
+    findContactByName(name: string): Contact[] {
         return this.contacts.filter(contact => contact.name === name);
     }
 
-    findContactByAddress(address) {
+    findContactByAddress(address: string): Contact[] {
         return this.contacts.filter(contact => contact.address === address);
     }
 
-    findContactByPhone(phoneNumber, phoneType: string) {
+    // phoneType : home, office, studio
+    findContactByPhone(phoneNumber: number, phoneType: PhoneType): Contact[] {
         return this.contacts.filter(
             contact => contact.phones[phoneType].num === phoneNumber
         );
     }
+    // findContactByPhone(PhoneType.Home);
 
-    addContact(contact) {
+    addContact(contact: Contact): void {
         this.contacts.push(contact);
     }
 
-    displayListByName() {
+    displayListByName(): string[] {
         return this.contacts.map(contact => contact.name);
     }
 
-    displayListByAddress() {
+    displayListByAddress(): string[] {
         return this.contacts.map(contact => contact.address);
     }
     /* ------------------------------------------------ */

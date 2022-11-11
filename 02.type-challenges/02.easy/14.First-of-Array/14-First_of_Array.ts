85308f19ad0cb93d2d8113d77d5bbc31d2fe5de4
yoo-jimin127 <dbwlals9936@naver.com>
2022-11-11 11:39:16 +0900

    fix : 14 source edit

--- 02.type-challenges/02.easy/14.First-of-Array/14-First_of_Array.ts
+++ 02.type-challenges/02.easy/14.First-of-Array/14-First_of_Array.ts
@@ -1,4 +1,4 @@
-type First<T extends any[]> =T extends [infer P, ...any] ? P : never;
+type First<T extends any[]> =T extends [] ? never : T[0];
 
 type arr1 = ["a", "b", "c"];
 type arr2 = [3, 2, 1];

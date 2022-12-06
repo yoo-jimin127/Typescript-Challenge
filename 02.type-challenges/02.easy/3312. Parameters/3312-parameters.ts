8d5e5de17c6b2fb7018275f02aadf16b014ebb17
Jimin Yoo <66112716+yoo-jimin127@users.noreply.github.com>
2022-12-06 19:07:49 +0900

    add : 3312-parameters answer

---
+++ 02.type-challenges/02.easy/3312. Parameters/3312-parameters.ts
@@ -0,0 +1,5 @@
+type MyParameters<T extends (...args: any[]) => any> =
+  T extends (...args: [...infer P]) => any ? P : never;
+
+const foo = (arg1: string, arg2: number): void => {}
+type FunctionParamsType = MyParameters<typeof foo> // [arg1: string, arg2: number]

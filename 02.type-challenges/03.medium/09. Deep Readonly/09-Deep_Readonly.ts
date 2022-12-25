f9dbf950ea14efdea2c484f109eb8b98286a41b0
yoo-jimin127 <dbwlals9936@naver.com>
2022-12-25 20:35:37 +0900

    add : 09-Deep_Readonly upload

---
+++ 02.type-challenges/03.medium/09. Deep Readonly/09-Deep_Readonly.ts
@@ -0,0 +1,22 @@
+type DeepReadonly<T> = {
+    readonly [P in keyof T]: T[P] extends Record<string, unknown>
+      ? DeepReadonly<T[P]> : T[P]; 
+  };
+
+type X = { 
+  x: { 
+    a: 1
+    b: 'hi'
+  }
+  y: 'hey'
+}
+  
+type Expected = { 
+  readonly x: { 
+    readonly a: 1
+    readonly b: 'hi'
+  }
+  readonly y: 'hey' 
+}
+
+type ReadonlyTodo = DeepReadonly<X> // should be same as `Expected`

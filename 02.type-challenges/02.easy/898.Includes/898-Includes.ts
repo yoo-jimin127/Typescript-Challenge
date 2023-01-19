2fa20119cdba8fb5dbdd536402054dd2811adcca
yoo-jimin127 <dbwlals9936@naver.com>
2023-01-19 17:39:54 +0900

    fix : 898-Includes syntax error debug

--- 02.type-challenges/02.easy/898.Includes/898-Includes.ts
+++
@@ -1,4 +0,0 @@
-type Includes<T extends unknown[], U> = U extends T[number] ? true : false;
-
-// expected to be `false`
-type isPillarMen = Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Dio">;

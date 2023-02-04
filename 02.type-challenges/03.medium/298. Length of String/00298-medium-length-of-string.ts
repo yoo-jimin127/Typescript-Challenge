6368f79496ea763a07ca592f9601c28279399622
yoo-jimin127 <dbwlals9936@naver.com>
2023-02-04 22:47:08 +0900

    add : 298-length-of-string src

---
+++ 02.type-challenges/03.medium/298. Length of String/00298-medium-length-of-string.ts
@@ -0,0 +1,16 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from '../../test-utils'
+
+type cases = [
+  Expect<Equal<LengthOfString<''>, 0>>,
+  Expect<Equal<LengthOfString<'kumiko'>, 6>>,
+  Expect<Equal<LengthOfString<'reina'>, 5>>,
+  Expect<Equal<LengthOfString<'Sound! Euphonium'>, 16>>,
+]
+
+
+// ============= Your Code Here =============
+type StringToArray<T extends string, S extends string[] = []> 
+  = T extends `${infer F}${infer Tail}` ? StringToArray<Tail, [...S, F]> : S
+
+type LengthOfString<S extends string> = StringToArray<S>['length'];
